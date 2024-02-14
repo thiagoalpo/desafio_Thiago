6705be9a52822593aa2f1afbc606350cd617bb91
thiagoalpo <159043919+thiagoalpo@users.noreply.github.com>
2024-02-13 21:32:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-let nome = "felipe"
-console.log(nome)
+// Definindo o nome e a quantidade de experiência (XP) do herói
+let nomeHeroi = "Thiago";
+let XPHeroi = 17001; // Altere esse valor para testar diferentes níveis
+
+// Utilizando uma estrutura de decisão para determinar o nível do herói com base na quantidade de XP
+let nivelHeroi;
+if (XPHeroi <= 1000) {
+    nivelHeroi = "Ferro";
+} else if (XPHeroi > 1000 && XPHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (XPHeroi > 2000 && XPHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (XPHeroi > 5000 && XPHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (XPHeroi > 7000 && XPHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (XPHeroi > 8000 && XPHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (XPHeroi > 9000 && XPHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+// Exibindo a mensagem com o nome e o nível do herói
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi);
+
 
